121d63da0411b0bcc7c91fc04a598ed977bdb9d2
henriquefantin <henrique.fantin.ns@gmail.com>
2020-04-29 22:23:31 -0300

    Alfa 1.0 - Tarefa 166

---
+++ database/migrations/2020_04_30_012032_create_produtos_vendas.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProdutosVendas extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produtos_vendas', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_produto');
+            $table->unsignedBigInteger('id_venda');
+            $table->timestamps();
+
+            $table->foreign('id_produto')->references('id')->on('produtos');
+            $table->foreign('id_venda')->references('id')->on('vendas');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produtos_vendas');
+    }
+}
