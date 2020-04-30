baede221f4d3cd5006c8c02d7fc4bc57cef927a1
matDIAS123 <matheusmatheus540@gmail.com>
2020-04-29 23:03:21 -0300

    Alfa 1.0 - Tarefa 165

--- app/Venda.php
+++ app/Venda.php
@@ -8,4 +8,12 @@ class Venda extends Model
 {
     protected $table = 'vendas';
     protected $primaryKey = 'id';
+
+    function usuario(){
+        return $this->belongsTo('App\Usuario', 'id_cliente', 'id');
+    }
+
+    function produtos(){
+        return $this->belongsToMany('App\Produto', 'produtos_vendas', 'id_venda', 'id_produto')->withPivot('id','quantidade', 'subtotal')->withTimestamps();
+    }
 }
