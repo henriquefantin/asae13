8e94c8ad9512d242df9ebbf87dbb4e64dae34150
gilsonbcastanheira@gmail.com <gilsonbcastanheira@gmail.com>
2020-04-29 22:01:11 -0300

    Alfa1.0-Tarefa162

--- app/Produto.php
+++ app/Produto.php
@@ -6,8 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Produto extends Model
 {
-    protected $table = 'produtos';
-    protected $primaryKey = 'id';
+   protected $table = 'produtos';
+   protected $primaryKey = 'id';
 
     function vendas(){
         return $this->belongsToMany('App\Venda', 'produtos_vendas', 'id_produto', 'id_venda')->withPivot('quantidade', 'subtotal')->withTimestamps();
